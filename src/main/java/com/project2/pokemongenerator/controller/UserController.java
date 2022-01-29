3874e5ba6a295ab24033679ec80bd9d0008c255c
David Doan <daviddoan45@gmail.com>
2022-01-29 00:59:56 -0500

    created User Service class for UserController

--- src/main/java/com/project2/pokemongenerator/controller/UserController.java
+++ src/main/java/com/project2/pokemongenerator/controller/UserController.java
@@ -1,4 +1,10 @@
 package com.project2.pokemongenerator.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth/users")
 public class UserController {
+
 }
