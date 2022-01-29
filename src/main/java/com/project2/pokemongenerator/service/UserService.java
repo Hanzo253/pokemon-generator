d2608f4ebf32253e6a41874b600d171edcf009b2
David Doan <daviddoan45@gmail.com>
2022-01-29 18:32:56 -0500

    created SecurityConfigurer class for authentication

--- src/main/java/com/project2/pokemongenerator/service/UserService.java
+++ src/main/java/com/project2/pokemongenerator/service/UserService.java
@@ -1,7 +1,9 @@
 package com.project2.pokemongenerator.service;
 
+import com.project2.pokemongenerator.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private UserRepository userRepository;
 }
