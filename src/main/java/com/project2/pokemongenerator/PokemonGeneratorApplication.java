fd63cf54c26b0965055696917d34630c9e09258a
David Doan <daviddoan45@gmail.com>
2022-01-29 00:16:32 -0500

    created pokemon generator project

---
+++ src/main/java/com/project2/pokemongenerator/PokemonGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.project2.pokemongenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonGeneratorApplication.class, args);
+    }
+
+}
