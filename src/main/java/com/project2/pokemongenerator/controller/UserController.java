124aacefb7abed7c5a24b4aeb24416b0253824b6
David Doan <daviddoan45@gmail.com>
2022-01-29 23:45:03 -0500

    added userService, authManager, and userDetailsService setters

--- src/main/java/com/project2/pokemongenerator/controller/UserController.java
+++ src/main/java/com/project2/pokemongenerator/controller/UserController.java
@@ -1,10 +1,31 @@
 package com.project2.pokemongenerator.controller;
 
+import com.project2.pokemongenerator.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth/users")
 public class UserController {
-    
+    private UserService userService;
+    private AuthenticationManager authenticationManager;
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Autowired
+    public void setAuthenticationManager(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Autowired
+    public void setUserDetailsService(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 }
