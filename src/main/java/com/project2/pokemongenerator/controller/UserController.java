a13288b5ae75d49e8f657432b431e9fbdaa8988c
David Doan <daviddoan45@gmail.com>
2022-01-30 00:21:33 -0500

    successfully implemented register and login feature with token authentication and authorization

--- src/main/java/com/project2/pokemongenerator/controller/UserController.java
+++ src/main/java/com/project2/pokemongenerator/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.project2.pokemongenerator.controller;
 
+import com.project2.pokemongenerator.model.Request.LoginRequest;
+import com.project2.pokemongenerator.model.User;
 import com.project2.pokemongenerator.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,13 +24,24 @@ public class UserController {
         this.userService = userService;
     }
 
-    @Autowired
-    public void setAuthenticationManager(AuthenticationManager authenticationManager) {
-        this.authenticationManager = authenticationManager;
+//    @Autowired
+//    public void setAuthenticationManager(AuthenticationManager authenticationManager) {
+//        this.authenticationManager = authenticationManager;
+//    }
+//
+//    @Autowired
+//    public void setUserDetailsService(UserDetailsService userDetailsService) {
+//        this.userDetailsService = userDetailsService;
+//    }
+
+    @PostMapping("/register")
+    public User createUser(@RequestBody User userObject) {
+        return userService.createUser(userObject);
     }
 
-    @Autowired
-    public void setUserDetailsService(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest) {
+        System.out.println("calling loginUser");
+        return userService.loginUser(loginRequest);
     }
 }
