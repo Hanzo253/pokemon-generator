b385dbcf181f602291cd7821dd0f0fc09bab1fbc
David Doan <daviddoan45@gmail.com>
2022-01-29 23:39:32 -0500

    created LoginRequest and LoginResponse classes for loginUser method

---
+++ src/main/java/com/project2/pokemongenerator/model/Response/LoginResponse.java
@@ -0,0 +1,17 @@
+package com.project2.pokemongenerator.model.Response;
+
+public class LoginResponse {
+    private String JWT;
+
+    public String getJWT() {
+        return JWT;
+    }
+
+    public LoginResponse(String JWT) {
+        this.JWT = JWT;
+    }
+
+    public void setJWT(String JWT) {
+        this.JWT = JWT;
+    }
+}
--- src/main/java/com/project2/pokemongenerator/service/UserService.java
+++ src/main/java/com/project2/pokemongenerator/service/UserService.java
@@ -1,10 +1,14 @@
 package com.project2.pokemongenerator.service;
 
+import com.project2.pokemongenerator.exceptions.InformationExistsException;
 import com.project2.pokemongenerator.model.User;
 import com.project2.pokemongenerator.repository.UserRepository;
 import com.project2.pokemongenerator.security.jwt.JWTUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -40,6 +44,16 @@ public class UserService {
     @Autowired
     public void setJwtUtils(JWTUtils jwtUtils) {
         this.jwtUtils = jwtUtils;
+    }
+
+    // create new user and save it in the database
+    public User createUser(User userObject) {
+        if (!userRepository.existsByEmailAddress(userObject.getEmailAddress())) {
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
+            return userRepository.save(userObject);
+        } else {
+            throw new InformationExistsException("user with email address " + userObject.getEmailAddress() + " already exists");
+        }
     }
 
     public User findUserByEmailAddress(String email) {
