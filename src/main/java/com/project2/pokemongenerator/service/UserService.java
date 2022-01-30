a5398653256b18837c7b34264360a243fa244fb5
David Doan <daviddoan45@gmail.com>
2022-01-29 23:31:27 -0500

    added passwordEncoder, authManager, userDetailsService, and jwtUtils setters

--- src/main/java/com/project2/pokemongenerator/service/UserService.java
+++ src/main/java/com/project2/pokemongenerator/service/UserService.java
@@ -2,7 +2,10 @@ package com.project2.pokemongenerator.service;
 
 import com.project2.pokemongenerator.model.User;
 import com.project2.pokemongenerator.repository.UserRepository;
+import com.project2.pokemongenerator.security.jwt.JWTUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +13,33 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
+    private AuthenticationManager authenticationManager;
+    private UserDetailsService userDetailsService;
+    private JWTUtils jwtUtils;
 
     @Autowired
     public void setUserRepository(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @Autowired
+    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Autowired
+    public void setAuthenticationManager(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Autowired
+    public void setUserDetailsService(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Autowired
+    public void setJwtUtils(JWTUtils jwtUtils) {
+        this.jwtUtils = jwtUtils;
     }
 
     public User findUserByEmailAddress(String email) {
