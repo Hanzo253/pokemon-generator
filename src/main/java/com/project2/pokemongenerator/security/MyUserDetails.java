44c138809332dcce84145e497bf3b69f12b21cea
David Doan <daviddoan45@gmail.com>
2022-01-29 22:02:47 -0500

    created MyUserDetails variables and constructors

--- src/main/java/com/project2/pokemongenerator/security/MyUserDetails.java
+++ src/main/java/com/project2/pokemongenerator/security/MyUserDetails.java
@@ -1,7 +1,20 @@
 package com.project2.pokemongenerator.security;
 
+import com.project2.pokemongenerator.model.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class MyUserDetails implements UserDetails {
 
+    private User user;
+    private String userName;
+    private String password;
+    private String emailAddress;
+
+    public MyUserDetails() {
+
+    }
+
+    public MyUserDetails(User user) {
+        this.user = user;
+    }
 }
