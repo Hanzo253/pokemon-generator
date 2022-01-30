22c55e079d9398ae12bb3c51bcc8b9ab41a3411b
David Doan <daviddoan45@gmail.com>
2022-01-29 21:53:28 -0500

    created users table and User field variables

--- src/main/java/com/project2/pokemongenerator/model/User.java
+++ src/main/java/com/project2/pokemongenerator/model/User.java
@@ -1,4 +1,25 @@
 package com.project2.pokemongenerator.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String userName;
+
+    @Column(unique = true)
+    private String emailAddress;
+
+    @Column
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
 }
