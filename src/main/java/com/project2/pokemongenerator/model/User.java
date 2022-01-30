e8f6aad8251b0a12884ea32241cd843741faa46d
David Doan <daviddoan45@gmail.com>
2022-01-29 21:59:46 -0500

    added User field variables' getters and setters

--- src/main/java/com/project2/pokemongenerator/model/User.java
+++ src/main/java/com/project2/pokemongenerator/model/User.java
@@ -33,4 +33,46 @@ public class User {
         this.emailAddress = emailAddress;
         this.password = password;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
