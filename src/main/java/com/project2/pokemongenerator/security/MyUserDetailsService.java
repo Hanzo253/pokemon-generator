4fa1d18733b4cb75c88ca14a87794ff096225e1b
David Doan <daviddoan45@gmail.com>
2022-01-29 22:25:26 -0500

    implemented loadUserByUsername method in MyUserDetailsService

--- src/main/java/com/project2/pokemongenerator/security/MyUserDetailsService.java
+++ src/main/java/com/project2/pokemongenerator/security/MyUserDetailsService.java
@@ -1,8 +1,8 @@
 package com.project2.pokemongenerator.security;
 
+import com.project2.pokemongenerator.model.User;
 import com.project2.pokemongenerator.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,6 +18,12 @@ public class MyUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-//        User user = userService
+        User user = userService.findUserByEmailAddress(email);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Username not found in database");
+        }
+        
+        return new MyUserDetails(user);
     }
 }
