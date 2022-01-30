5f58d3ca40d696247af46eec6ecc5632659ee13b
David Doan <daviddoan45@gmail.com>
2022-01-29 22:20:14 -0500

    added userRepository setter in UserService

--- src/main/java/com/project2/pokemongenerator/security/MyUserDetailsService.java
+++ src/main/java/com/project2/pokemongenerator/security/MyUserDetailsService.java
@@ -1,4 +1,23 @@
 package com.project2.pokemongenerator.security;
 
-public class MyUserDetailsService {
+import com.project2.pokemongenerator.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class MyUserDetailsService implements UserDetailsService {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+//        User user = userService
+    }
 }
--- src/main/java/com/project2/pokemongenerator/service/UserService.java
+++ src/main/java/com/project2/pokemongenerator/service/UserService.java
@@ -1,9 +1,20 @@
 package com.project2.pokemongenerator.service;
 
 import com.project2.pokemongenerator.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private UserRepository userRepository;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+//    public User findUserByEmailAddress(String email) {
+//        return userRepository.findUserByEmailAddress(email);
+//    }
 }
