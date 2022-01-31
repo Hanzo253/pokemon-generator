84c8c16bd5ba70d61bf1019c7c7c74b355bd7e0f
David Doan <daviddoan45@gmail.com>
2022-01-30 23:45:33 -0500

    implemented createPokemon and getAllPokemon method with PokemonService in PokemonController

--- src/main/java/com/project2/pokemongenerator/controller/PokemonController.java
+++ src/main/java/com/project2/pokemongenerator/controller/PokemonController.java
@@ -1,9 +1,11 @@
 package com.project2.pokemongenerator.controller;
 
+import com.project2.pokemongenerator.model.Pokemon;
 import com.project2.pokemongenerator.service.PokemonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api") // means http://localhost:9092/api/
@@ -13,5 +15,17 @@ public class PokemonController {
     @Autowired
     public void setPokemonService(PokemonService pokemonService) {
         this.pokemonService = pokemonService;
+    }
+
+    @PostMapping("/pokemon/")
+    public Pokemon createPokemon(@RequestBody Pokemon pokemonObject) {
+        System.out.println("creating new pokemon....");
+        return pokemonService.createPokemon(pokemonObject);
+    }
+
+    @GetMapping("/pokemon/")
+    public List<Pokemon> getAllPokemon() {
+        System.out.println("getting all pokemon...");
+        return pokemonService.getAllPokemon();
     }
 }
